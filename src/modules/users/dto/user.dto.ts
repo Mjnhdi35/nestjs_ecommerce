ccf22a7f03b1a2740db2f5967aefa2ce04283193
mdjubuntu <mjnhdi35@gmail.com>
2025-09-10 02:02:58 +0700

    Enhance user module with TypeORM integration and additional fields. Added UsersModule, updated user entity to include a default display name, and modified DTOs to include displayName for user creation and updates. Updated package dependencies in package-lock.json and yarn.lock for new modules.

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -4,17 +4,27 @@ export class CreateUsersDto {
   @IsString()
   @IsNotEmpty()
   username: string;
+
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
+
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  displayName: string;
 }
 export class UpdateUsersDto {
   @IsString()
   @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  displayName: string;
 }
